0f0b749a82e2028a5d30a51a8ba037e42ac4051d
RBasic <rcham.pro@gmail.com>
2023-03-15 14:42:31 +0100

    Initial commit

---
+++ Assets/Bullet.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    Rigidbody2D rb;
+    Vector2 direction;
+    float speed = 500f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        direction = transform.right;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.velocity = (direction * speed * Time.fixedDeltaTime);
+        Debug.Log("direction = "+direction+" Speed = "+speed);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Enemy")
+        {
+            Destroy(collision.gameObject);
+        }
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Movement2D.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+using UnityEngine.U2D;
+using static UnityEngine.GraphicsBuffer;
+
+public class Movement2D : MonoBehaviour
+{
+    float horizontalValue;
+    Rigidbody2D rb;
+    Vector3 m_Velocity = Vector3.zero; 
+    [SerializeField] private float m_MovementSmoothing = .05f;
+    float speed = 40f;
+    private float m_JumpForce = 800f;
+    private bool jumping = false;
+    public GameObject pivotGun;
+    public GameObject spawnPoint;
+    public Object bulletRef;
+
+    SpriteRenderer sr;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalValue = Input.GetAxis("Horizontal");
+        if (Input.GetButtonDown("Jump"))
+        {
+            jumping = true;
+        }
+        Vector3 target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 v_diff = (target - transform.position).normalized;
+        float atan2 = Mathf.Atan2(v_diff.y, v_diff.x);
+        pivotGun.transform.rotation = Quaternion.Euler(0f, 0f, atan2 * Mathf.Rad2Deg);
+
+        Debug.Log(v_diff.x);
+
+        sr.flipX = (v_diff.x < 0);
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Debug.Log("Fire !");
+            Instantiate(bulletRef,spawnPoint.transform.position, pivotGun.transform.rotation);
+        }
+
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 targetVelocity = new Vector2(horizontalValue * 10f * speed * Time.fixedDeltaTime, rb.velocity.y);
+        rb.velocity = Vector3.SmoothDamp(rb.velocity, targetVelocity, ref m_Velocity, m_MovementSmoothing);
+        if (jumping)
+        {
+            rb.AddForce(new Vector2(0f, m_JumpForce));
+        }
+        jumping = false;
+    }
+}
